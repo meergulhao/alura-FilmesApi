fb41f82e7b22215d98b89693957d1714af45e5f6
Guilherme Mergulhão <golimpiof@gmail.com>
2026-01-16 14:18:34 -0300

    Adiciona método GET com paginação, e ajusta retornos

--- Models/Filme.cs
+++ Models/Filme.cs
@@ -1,12 +1,14 @@
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 
 namespace FilmesApi.Models;
 
 public class Filme
 {
+    public int Id { get; internal set; }
     [Required(ErrorMessage = "O título do filme é obrigatório.")]
     public string Titulo { get; set; }
-    [Required(ErrorMessage = "O nome do diretor é obrigatório.")]
+    [Required(ErrorMessage = "O diretor do filme é obrigatório.")]
     public string Diretor { get; set; }
     [Required(ErrorMessage = "O gênero do filme é obrigatório.")]
     [MaxLength(50, ErrorMessage = "O tamanho do gênero não pode exceder 50 caracteres.")]
